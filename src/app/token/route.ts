b4ddb35702e84ec967a92bf5c95f073805c65017
Afsal Khan <afsalkhanmdt@gmail.com>
2025-04-13 11:52:06 +0530

    fix: add access token

--- src/app/token/route.ts
+++ src/app/token/route.ts
@@ -3,6 +3,7 @@ import { LoginSchema } from "./dto";
 import dbConnect from "@/core/db/connect";
 import User from "@/models/users";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 function normalizeKeys(obj: Record<string, unknown>): Record<string, unknown> {
     const normalized: Record<string, unknown> = {};
@@ -38,5 +39,31 @@ export async function POST(
             "error_description": "invalid credential"
         }, { status: 400 });
     }
-    return Response.json({ Promo: 0, Month: 0, FreeTrialPeriod: 30 / 30 });
+    const token = jwt.sign({ id: user._id }, "test", { expiresIn: '1h' });
+    return Response.json({
+        access_token: token,
+        token_type: "bearer",
+        expires_in: 1 * 60 * 60 - 1,
+        userName: user.email,
+        familyId: "48173",
+        validateTillDate: new Date(
+            Date.now() + 3600 * 1000
+        ).toLocaleString('en-US', {
+            timeZone: 'Europe/Copenhagen',
+            month: 'numeric',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true,
+        }),
+        memberType: "0",
+        promo: "0",
+        memberId: user._id,
+        countryCode: "",
+        showPopup: "False",
+        ".issued": new Date().toUTCString(),
+        ".expires": new Date(Date.now() + 3600 * 1000).toUTCString(),
+    });
 }
