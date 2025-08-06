c3aa2181743d0042655dbaf2b3a0573c7013644d
Afsal Khan <afsalkhanmdt@gmail.com>
2025-08-07 03:31:20 +0530

    middle ware edited

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,33 +19,20 @@ const allowlist = [
     '/api/account/register',
     '/token',
     '/admin/login',
-];
-
-
-const isAllowlisted = (pathname: string) => {
-    return allowlist.some((pattern) => {
-        if (pattern.endsWith('*')) {
-            return pathname.startsWith(pattern.slice(0, -1));
-        }
-        return pathname === pattern;
-    });
-};
+]
 
 export async function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
-
-    
-    if (isAllowlisted(pathname)) {
-        return NextResponse.next();
-    }
-
-
+    //Check the user is authenticated
+    const url = request.nextUrl
+    const pathname = url.pathname
     if ((pathname.startsWith('/api') || pathname.startsWith('/Token')) && pathname !== pathname.toLowerCase()) {
-        const lowercaseURL = request.nextUrl.clone();
-        lowercaseURL.pathname = pathname.toLowerCase();
-        return NextResponse.redirect(lowercaseURL);
+        const lowercaseURL = url.clone()
+        lowercaseURL.pathname = pathname.toLowerCase()
+        return NextResponse.redirect(lowercaseURL)
     }
-
+    if (allowlist.some((path) => pathname === path)) {
+        return NextResponse.next()
+    }
     const accessToken = request.headers.get('authorization')?.split(' ')[1];
     if (!accessToken) {
         return NextResponse.json({ message: 'Unauthorized', status: 401 }, { status: 401 });
@@ -64,6 +51,6 @@ export async function middleware(request: NextRequest) {
     return response;
 }
 
-// export const config = {
-//     matcher: '/api/:path*',
-// }
+export const config = {
+    matcher: '/api/:path*',
+}
