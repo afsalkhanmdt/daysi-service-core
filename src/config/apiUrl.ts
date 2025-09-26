3c0113a0c3b09b2d7fc26e05355eb0727513f396
soorajUM <soorajushamurali13@gmail.com>
2025-09-26 11:20:35 +0530

    changing to production environment

--- src/config/apiUrl.ts
+++ src/config/apiUrl.ts
@@ -2,8 +2,9 @@ const ENVIRONMENT = process.env.NEXT_PUBLIC_ENVIRONMENT || "prod";
 const localSever = "localhost:8000";
 const apiSever = "api.daysi.dk";
 const devSever = "dev.daysi.dk";
+const prodSever = "api.daysi.dk";
 
-const apiUrl =`https://${devSever}/api/`
+const apiUrl =`https://${prodSever}/api/`
           ;
 
 export const assetsUrl = "https://assets.babble-ai.com";
