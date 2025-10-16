16da28587c7d04d7c125501c1059bc7a37683eb2
soorajUM <soorajushamurali13@gmail.com>
2025-10-16 14:13:21 +0530

    api url changed

--- src/config/apiUrl.ts
+++ src/config/apiUrl.ts
@@ -1,7 +1,7 @@
 const ENVIRONMENT = process.env.NEXT_PUBLIC_ENVIRONMENT || "prod";
 const localSever = "localhost:8000";
 const apiSever = "api.daysi.dk";
-const devSever = "dev.daysi.dk";
+const devSever = "api.daysi.dk";
 const prodSever = "api.daysi.dk";
 
 const apiUrl =`https://${devSever}/api/`
