365758d2f51f9dd8219e3733fcba26e5e9665388
soorajUM <soorajushamurali13@gmail.com>
2025-10-21 10:16:58 +0530

    favicon edited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import icon from "../../public/favicon.ico";
 
 export const metadata: Metadata = {
   title: "Daysi",
   description: "Daysi Calender Application",
   icons: {
-    icon: "../../public/favicon.ico",
+    icon: "/favicon.ico",
   },
 };
 
