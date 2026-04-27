18b7266dff3000e5e1e160f86699000ad1ebe1d3
soorajUM <soorajushamurali13@gmail.com>
2026-04-27 13:32:35 +0530

    post call edit

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
 import { postCall } from "@/services/api/apiCall";
-export const createAppointmentCall = postCall("/Events/Create");
-export const updateAppointmentCall = postCall("/Events/Update");
+export const createAppointmentCall = postCall("Events/Create");
+export const updateAppointmentCall = postCall("Events/Update");
 export const createPocketMoneyTaskCall = postCall("PocketMoney/CreatePMTask");
 export const updatePocketMoneyTaskCall = postCall("PocketMoney/UpdatePMTask");
-export const createToDoTaskCall = postCall("/ToDo/CreateToDo");
-export const updateToDoTaskCall = postCall("/ToDo/UpdateTodo");
+export const createToDoTaskCall = postCall("ToDo/CreateToDo");
+export const updateToDoTaskCall = postCall("ToDo/UpdateTodo");
